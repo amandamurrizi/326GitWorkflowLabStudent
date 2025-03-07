c8e47e1155be08855a644102d81a6b898e2ed43c
abyy04 <160357900+abyy04@users.noreply.github.com>
2025-03-07 14:39:25 -0500

    implemented reciprocal function

--- calculator.js
+++ calculator.js
@@ -15,10 +15,15 @@ function calculateSquare(value) {
 
 // TODO: FIX reciprocal function
 function calculateReciprocal() {
+    let input = document.getElementById("display").value; 
+    if (input == 0) {
+        alert("Cannot divide by zero!"); 
+        return;
+    }
+    let result = 1 / parseFloat(input); 
+    document.getElementById("display").value = result;
     // Hint: Reciprocal is 1/x
     // return the square root of the value
-    return 1* value;
-
 }
 
 // TODO: Implement natural log function
